a0729c3cda7d9192dedbc989697fe617d7b853c6
deltin00 <deltin00@gmail.com>
2018-09-29 19:51:18 -0400

    front end is responsive

--- Tic-Tac-Toe-client/assets/scripts/game/ui.js
+++ Tic-Tac-Toe-client/assets/scripts/game/ui.js
@@ -2,62 +2,10 @@
 
 const store = require('../store.js')
 
-// const signUpSuccess = function () {
-//   $('#display-message').html('Sign up successful')
-// }
-// const signUpFailure = function () {
-//   $('#display-message').html('Something went wrong, please try again')
-//   $('#display-message').css('color', 'red')
-//   $('#sign-up-form').trigger('reset')
-// }
-// const signInSuccess = function (response) {
-//   $('#display-message').html('Sign in successful')
-//   $('#display-message').css('color', 'green')
-//   $('#sign-in-form').trigger('reset')
-//   console.log('store before adding user key', store)
-//   store.user = response.user
-//   console.log('store after adding user key', store)
-//   console.log('store.user.token', store.user.token)
-//   $('#sign-up-form').addClass('hidden')
-//   $('#sign-in-form').addClass('hidden')
-//   $('#Change-password-form').addClass('hidden')
-//   // $('#sign-out-button').addClass('hidden')
-// }
-// const signInFailure = function () {
-//   $('#display-message').html('Something went wrong, please try again')
-//   $('#display-message').css('color', 'red')
-//   $('#sign-in-form').trigger('reset')
-// }
-// const signOutSuccess = function () {
-//   $('#display-message').html('Sign Out successful')
-//   $('#display-message').css('color', 'green')
-//   $('#sign-up-form').removeClass('hidden')
-//   $('#sign-in-form').removeClass('hidden')
-//   $('#change-password-form').addClass('hidden')
-//   $('#sign-out-button').addClass('hidden')
-// }
-// const signOutFailure = function () {
-//   $('#display-message').html('Something went wrong, please try again')
-//   $('#display-message').css('color', 'red')
-//   $('#sign-in-form').trigger('reset')
-// }
 const gameCreateSuccess = function (response) {
   console.log('success creating game')
   console.log(response)
   store.game = response
-  // const gameObjectForHTML = response
-  // const gameObjectForHTML = (`
-  //         cells: ${gameObjectForHTML.cells[0]}</h4>
-  //         <p>Author: ${book.author}</p>
-  //         <p>ID: ${book.id}</p>
-  //         <br />
-  //       `)
-  //   $('#content').append(bookHTML)
-
-  // $('#sign-up-form').addClass('hidden')
-  // $('#sign-in-form').addClass('hidden')
-  // $('#Change-password-form').addClass('hidden')
-  // $('#sign-out-button').addClass('hidden')
 }
 const gameCreatefailure = function () {
   $('#display-message').html('Game not working')
@@ -74,39 +22,29 @@ const gameXmove = function (data) {
   console.log(data.game.id)
   console.log(data.game.player_x.id)
   console.log(data.game.cells)
-//  console.log(data.game.cells[moveData.target.id])
-  // {
-  // "game": {
-  //   "id": 3,
-  //   "cells": ["","","","","","","","",""],
-  //   "over": false,
-  //   "player_x": {
-  //     "id": 1,
-  //     "email": "and@and.com"
-  //   },
-  //   "player_o": null
-  // }
-  // }
-  // empty content element
-//  $('#content').html('')
-  // // loop through API response date
-  // const book = response.book
-  //  const gameHTML = (`
-  //           .0: ${book.title}</h4>
-  // //       <p>Author: ${book.author}</p>
-  // //       <p>ID: ${book.id}</p>
-  //       $('#sign-in-form').trigger('reset')
-  // //     `)
-  // // $('#content').append(bookHT
+  updateboard(data)
 }
 const gameXmovefail = function () {
   $('#display-message').html('Wrong move!')
   $('#display-message').css('color', 'red')
   console.log('game x move failed in event')
 }
+const updateboard = function (data) {
+  console.log('in updateboard UI' + data.game.cells)
+  $('#0').html(data.game.cells[0])
+  $('#1').html(data.game.cells[1])
+  $('#2').html(data.game.cells[2])
+  $('#3').html(data.game.cells[3])
+  $('#4').html(data.game.cells[4])
+  $('#5').html(data.game.cells[5])
+  $('#6').html(data.game.cells[6])
+  $('#7').html(data.game.cells[7])
+  $('#8').html(data.game.cells[8])
+}
 module.exports = {
   gameCreateSuccess,
   gameCreatefailure,
   gameXmove,
-  gameXmovefail
+  gameXmovefail,
+  updateboard
 }
